ba091c512725281ec0abf6372eea2ba448a8dc0a
Bruce Xie <bruce.xie@fedilitylife.co.nz>
2024-08-06 23:45:46 +1200

    adding outputs which I missed from the first commit

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "instance_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "instance_public_dns" {
+  description = "The public DNS of the EC2 instance"
+  value       = aws_instance.web.public_dns
+}
